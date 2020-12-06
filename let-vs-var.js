a189f12102de2b5e39c742dcb426f5b92678d8ad
Alfredo Aguirre <thefurok@gmail.com>
2020-12-06 02:00:11 -0500

    fundamentos de NodeJS

---
+++ let-vs-var.js
@@ -0,0 +1,19 @@
+// var nombre = "Hola Rodrigo!";
+let nombre = "Rodrigo";
+
+if (true){
+    let nombre = "Alfredo";
+    console.log(nombre);
+}
+
+//let nombre = "Nicolas";
+
+console.log(nombre);
+
+let i = 8;
+
+for (let i=0; i <= 5; i++){
+    console.log(`i = ${i}`);
+}
+
+console.log(i);
